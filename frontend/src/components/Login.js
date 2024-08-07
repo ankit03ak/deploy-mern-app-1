479bdf2f25095da5bff4cad06299a6bcb1e0ade4
ankit03ak <124338055+ankit03ak@users.noreply.github.com>
2024-08-07 12:37:25 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -28,7 +28,7 @@ const Login = () => {
             return handleError('name , email and password required')
         }
         try {
-            const url = "http://localhost:8080/auth/login"
+            const url = "https://deploy-mern-app-1-api1.vercel.app/auth/login"
             const response = await fetch(url, {
                 method : "POST",
                 headers : {
